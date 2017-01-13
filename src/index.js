5d36f2e040f5842d4543de23189b47e53377f3e9
Xavi <mrjavoman@hotmail.com>
2017-01-12 20:58:24 -0600

    addding container and reduceres to app

--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { combineReducers } from 'redux';
-import booksReducer from './reducers/reducer_books'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
+import App from './components/app';
+import reducers from './reducers';
 
-const rootReducer = combineReducers({
-    books: booksReducer
-});
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-export default rootReducer;
-
-// // Create a new component. This component should produce some HTML
-// const App = () => {
-
-//     return (
-//         <div>
-//             Hello World!
-//         </div>
-//     );
-// }
-
-// // Take Component and put it in the DOM
-// ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
